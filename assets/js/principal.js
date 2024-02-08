aa9ce13bd0a544421ece72af050e154f7bab14c2
redemare <redemare23@gmail.com>
2024-02-07 23:42:20 -0300

    continuando integração do front com beck no oracle

--- assets/js/principal.js
+++ assets/js/principal.js
@@ -1,32 +1,34 @@
-// URL da API
-const apiUrl = 'https://apex.oracle.com/pls/apex/redemare/site/texto';
+document.addEventListener("DOMContentLoaded", function () {
+  // URL da API
+  const apiUrl = 'https://apex.oracle.com/pls/apex/redemare/conteudos/textos';
 
-// Fazendo uma solicitação GET usando Fetch
-fetch(apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Erro ao recuperar os dados da API');
-    }
-    return response.json();
-  })
-  .then(data => {
-    // Verificar a estrutura dos dados
-    console.log(data);
+  // Fazendo uma solicitação GET usando Fetch
+  fetch(apiUrl)
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Erro ao recuperar os dados da API');
+          }
+          return response.json();
+      })
+      .then(data => {
+          // Manipular e exibir os dados na página HTML
+          console.log(data);
+          displayData(data.items);
+      })
+      .catch(error => {
+          console.error('Erro durante a solicitação:', error);
+      });
 
-    // Acessar a matriz de funcionários
-    const conteudoPrincipal = data.items;
+  // Função para exibir os dados na página HTML
+  function displayData(items) {
+    // Exibindo o conteúdo principal
+    const titulo = document.getElementById('titulo');
+    const conteudoPrincipal = document.getElementById('conteudo-principal');
 
-    // Manipular os dados recebidos e inserir na lista
-    const employeeList = document.getElementById('conteudo-principal');
+    const tituloMapa = document.getElementById('titulo-mapa');
+    const conteudoMapa = document.getElementById('conteudo-mapa');
 
-    conteudoPrincipal.forEach(conten_text => {
-        const listItem = document.createElement('p');
-        listItem.textContent = `${conten_text.content_text}`;
-        employeeList.appendChild(listItem);
-    });
-  })
-  .catch(error => {
-    console.error('Erro durante a solicitação:', error);
-  });
-
-  
+    tituloMapa.innerHTML = items[2].titulo;
+    conteudoMapa.innerHTML = items[2].conteudo;
+  }
+});
