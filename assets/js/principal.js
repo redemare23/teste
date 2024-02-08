52073a2dae649dc184321e2abd3a5bafce2007bf
redemare <redemare23@gmail.com>
2024-02-08 20:46:50 -0300

    textos iniciais totalmentes integrados

--- assets/js/principal.js
+++ assets/js/principal.js
@@ -25,12 +25,31 @@ document.addEventListener("DOMContentLoaded", function () {
     const titulo = document.getElementById('titulo');
     const conteudoPrincipal = document.getElementById('conteudo-principal');
 
-    const tituloMapa = document.getElementById('titulo-mapa');
-    const conteudoMapa = document.getElementById('conteudo-mapa');
+    const tituloMapa1 = document.getElementById('titulo-mapa1');
+    const conteudoMapa1 = document.getElementById('conteudo-mapa1');
+
+    const tituloMapa2 = document.getElementById('titulo-mapa2');
+    const conteudoMapa2 = document.getElementById('conteudo-mapa2');
+
+    const mangueTitulo = document.getElementById('Mangue-titulo');
+    const mangueConteudo = document.getElementById('Mangue-conteudo');
+
+    const restigaTitulo = document.getElementById('Restiga-titulo');
+    const restigaConteudo = document.getElementById('Restiga-conteudo');
 
     titulo.innerHTML = items[0].titulo;
     conteudoPrincipal.innerHTML = items[0].conteudo;
-    tituloMapa.innerHTML = items[2].titulo;
-    conteudoMapa.innerHTML = items[2].conteudo;
+
+    tituloMapa1.innerHTML = items[2].titulo;
+    conteudoMapa1.innerHTML = items[2].conteudo;
+
+    tituloMapa2.innerHTML = items[3].titulo;
+    conteudoMapa2.innerHTML = items[3].conteudo;
+
+    mangueTitulo.innerHTML = items[4].titulo;
+    mangueConteudo.innerHTML = items[4].conteudo;
+
+    restigaTitulo.innerHTML = items[1].titulo;
+    restigaConteudo.innerHTML = items[1].conteudo;
   }
 });
