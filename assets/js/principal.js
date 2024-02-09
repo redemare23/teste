5015ef4ec679c6aedd10a08791a3543e9050f35f
redemare <redemare23@gmail.com>
2024-02-08 22:03:16 -0300

    refatorando para uma organização melhor

--- assets/js/principal.js
+++ assets/js/principal.js
@@ -1,55 +1,40 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // URL da API
-  const apiUrl = 'https://apex.oracle.com/pls/apex/redemare/conteudos/textos';
-
-  // Fazendo uma solicitação GET usando Fetch
-  fetch(apiUrl)
-      .then(response => {
-          if (!response.ok) {
-              throw new Error('Erro ao recuperar os dados da API');
-          }
-          return response.json();
-      })
-      .then(data => {
-          // Manipular e exibir os dados na página HTML
-          console.log(data);
-          displayData(data.items);
-      })
-      .catch(error => {
-          console.error('Erro durante a solicitação:', error);
-      });
-
-  // Função para exibir os dados na página HTML
-  function displayData(items) {
-    // Exibindo o conteúdo principal
-    const titulo = document.getElementById('titulo');
-    const conteudoPrincipal = document.getElementById('conteudo-principal');
-
-    const tituloMapa1 = document.getElementById('titulo-mapa1');
-    const conteudoMapa1 = document.getElementById('conteudo-mapa1');
-
-    const tituloMapa2 = document.getElementById('titulo-mapa2');
-    const conteudoMapa2 = document.getElementById('conteudo-mapa2');
-
-    const mangueTitulo = document.getElementById('Mangue-titulo');
-    const mangueConteudo = document.getElementById('Mangue-conteudo');
-
-    const restigaTitulo = document.getElementById('Restiga-titulo');
-    const restigaConteudo = document.getElementById('Restiga-conteudo');
-
-    titulo.innerHTML = items[0].titulo;
-    conteudoPrincipal.innerHTML = items[0].conteudo;
-
-    tituloMapa1.innerHTML = items[2].titulo;
-    conteudoMapa1.innerHTML = items[2].conteudo;
-
-    tituloMapa2.innerHTML = items[3].titulo;
-    conteudoMapa2.innerHTML = items[3].conteudo;
-
-    mangueTitulo.innerHTML = items[4].titulo;
-    mangueConteudo.innerHTML = items[4].conteudo;
-
-    restigaTitulo.innerHTML = items[1].titulo;
-    restigaConteudo.innerHTML = items[1].conteudo;
-  }
-});
+document.addEventListener("DOMContentLoaded", async function () {
+    try {
+      const apiUrl = 'https://apex.oracle.com/pls/apex/redemare/conteudos/textos';
+      const response = await fetch(apiUrl);
+  
+      if (!response.ok) {
+        throw new Error('Erro ao recuperar os dados da API');
+      }
+  
+      const data = await response.json();
+      displayData(data.items);
+    } catch (error) {
+      console.error('Erro durante a solicitação:', error.message);
+    }
+  
+    function displayData(items) {
+      const updateElement = (id, content) => {
+        const element = document.getElementById(id);
+        if (element) {
+          element.innerHTML = content;
+        }
+      };
+  
+      updateElement('titulo', items[0].titulo);
+      updateElement('conteudo-principal', items[0].conteudo);
+  
+      updateElement('titulo-mapa1', items[2].titulo);
+      updateElement('conteudo-mapa1', items[2].conteudo);
+  
+      updateElement('titulo-mapa2', items[3].titulo);
+      updateElement('conteudo-mapa2', items[3].conteudo);
+  
+      updateElement('Mangue-titulo', items[4].titulo);
+      updateElement('Mangue-conteudo', items[4].conteudo);
+  
+      updateElement('Restiga-titulo', items[1].titulo);
+      updateElement('Restiga-conteudo', items[1].conteudo);
+    }
+  });
+  
